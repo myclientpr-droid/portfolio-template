f202116ea4eaf3af6d6b3680bccee1b0fa373739
mdaleardc <ismailaliamot@gmail.com>
2025-11-22 11:34:36 +0600

    add GSC, GTA4 and GAC

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,24 +2,49 @@ import { Inter } from 'next/font/google';
 import { Navigation } from '@/components/layouts/Navigation';
 import { Footer } from '@/components/layouts/Footer';
 import './globals.css';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Portfolio Template',
+  title: 'Writer Noor Aziz',
   description: 'A modern portfolio template built with Next.js',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="HHEUeoU41cwIxQKZGJ_siRAyE6uPZ6J905DwzDN0ZMY" />
+
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4649419420895287"
+          crossOrigin="anonymous"
+        />
+
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-M8T36YDCKY"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M8T36YDCKY');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Navigation />
         <main>{children}</main>
-        <Analytics/>
-        <SpeedInsights/>
+        <Analytics />
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
